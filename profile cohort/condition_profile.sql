a86a2aa6be4df74fa5f3466a52158b909976d88b
matthewlrobinson <69312120+matthewlrobinson@users.noreply.github.com>
2023-06-07 06:00:03 -0400

    Use curated tables instead of entire OMOP instance and remove visit_occurence_id
    This script now points to the tables generated by the 02_CURE_ID_All_Tables.sql script which removes the need to re-identify conditions of interest by their temporal relationship to the cohort. Some additional edits made for readability.

--- profile cohort/condition_profile.sql
+++ profile cohort/condition_profile.sql
@@ -5,20 +5,12 @@
 select 
     concept_name as condition_name
     ,condition_concept_id 
-    ,count(distinct coh.person_id) as persons_with_condition
-    ,100 * count(distinct coh.person_id) / (select count(distinct person_id) from results.CURE_ID_Cohort) as percent_persons_with_condition
+    ,count(distinct c.person_id) as persons_with_condition
+    ,100 * count(distinct c.person_id) / (select count(distinct person_id) from results.CURE_ID_Cohort) as percent_persons_with_condition
 from 
-    results.CURE_ID_Cohort coh
-left join
-    CONDITION_OCCURRENCE on
-        CONDITION_OCCURRENCE.person_id = coh.person_id   
-        AND (
-      CONDITION_OCCURRENCE.condition_start_date < coh.visit_end_date
-      OR CONDITION_OCCURRENCE.condition_end_date > coh.visit_start_date
-      OR CONDITION_OCCURRENCE.condition_end_date IS NULL
-      )
+    Results.CURE_ID_Condition_Occurrence c
 left join 
-    CONCEPT on concept.concept_id = CONDITION_OCCURRENCE.condition_concept_id 
+    CONCEPT on concept.concept_id = c.condition_concept_id 
 group by
     condition_concept_id 
     ,concept_name
@@ -30,21 +22,13 @@ order by persons_with_condition desc
 select 
     concept_name as condition_name
     ,ancestor_concept_id 
-    ,count(distinct coh.person_id) as persons_with_condition
-    ,100 * count(distinct coh.person_id) / (select count(distinct person_id) from results.CURE_ID_Cohort) as percent_persons_with_condition
+    ,count(distinct c.person_id) as persons_with_condition
+    ,100 * count(distinct c.person_id) / (select count(distinct person_id) from results.CURE_ID_Cohort) as percent_persons_with_condition
 from 
-    results.CURE_ID_Cohort coh
-left join
-    CONDITION_OCCURRENCE on
-        CONDITION_OCCURRENCE.person_id = coh.person_id   
-        AND (
-      CONDITION_OCCURRENCE.condition_start_date < coh.visit_end_date
-      OR CONDITION_OCCURRENCE.condition_end_date > coh.visit_start_date
-      OR CONDITION_OCCURRENCE.condition_end_date IS NULL
-      )
+   Results.CURE_ID_Condition_Occurrence c
 left join
     CONCEPT_ancestor on
-        descendant_concept_id = CONDITION_OCCURRENCE.condition_concept_id
+        descendant_concept_id = c.condition_concept_id
 left join 
     concept on concept.concept_id = CONCEPT_ancestor.ancestor_concept_id 
 where
