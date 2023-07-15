29adc5be9805afe57bf88a1daa5114da8fa2170a
matthewlrobinson <69312120+matthewlrobinson@users.noreply.github.com>
2023-07-14 23:14:14 -0400

    Update 07_A_condition_profile.sql
    The prior version of the first block of this script counted only persons with exact matches to condition_concept_id's. For conditions recorded that were recorded in a fashion less specific than common practice, these conditions appeared to be uncommon. For example, tonsillitis may have been counted as a rare condition if most of the time patients were categorized as acute tonsillitis. This revision includes descendant concepts as well.

--- Cohort curation scripts/07_A_condition_profile.sql
+++ Cohort curation scripts/07_A_condition_profile.sql
@@ -4,20 +4,25 @@
 
 USE YOUR_DATABASE;
 
----Profile condition prevalence in cohort by individual low-level condition concept
+---Profile condition prevalence in cohort by individual condition concept
+---Condition counts include descendants 
 select 
     concept_name as condition_name
-    ,condition_concept_id 
-    ,count(distinct c.person_id) as persons_with_condition
-    ,100 * count(distinct c.person_id) / (select count(distinct person_id) from results.CURE_ID_Cohort) as percent_persons_with_condition
+    ,ancestor_concept_id 
+    ,count(distinct c.person_id) as persons_with_condition_or_descendant
+    ,100 * count(distinct c.person_id) / (select count(distinct person_id) from [Results].[deident_CURE_ID_Person]) as percent_persons_with_condition
 from 
     [Results].[CURE_ID_Condition_Occurrence_Rare_Removed] c
-left join 
-    CONCEPT on concept.concept_id = c.condition_concept_id 
+    inner join CONCEPT_ANCESTOR
+        on descendant_concept_id = c.condition_concept_id
+        and ancestor_concept_id in 
+            (select condition_concept_id from [Results].[CURE_ID_Condition_Occurrence_Rare_Removed])
+left join CONCEPT
+  on concept.concept_id = CONCEPT_ANCESTOR.ancestor_concept_id 
 group by
-    condition_concept_id 
+    ancestor_concept_id 
     ,concept_name
-order by persons_with_condition desc
+order by persons_with_condition_or_descendant desc
 
  
 
@@ -26,14 +31,14 @@ select 
     concept_name as condition_name
     ,ancestor_concept_id 
     ,count(distinct c.person_id) as persons_with_condition
-    ,100 * count(distinct c.person_id) / (select count(distinct person_id) from results.CURE_ID_Cohort) as percent_persons_with_condition
+    ,100 * count(distinct c.person_id) / (select count(distinct person_id) from [Results].[deident_CURE_ID_Person]) as percent_persons_with_condition
 from 
    [Results].[CURE_ID_Condition_Occurrence_Rare_Removed] c
 left join
-    CONCEPT_ancestor on
+    CONCEPT_ANCESTOR on
         descendant_concept_id = c.condition_concept_id
 left join 
-    concept on concept.concept_id = CONCEPT_ancestor.ancestor_concept_id 
+    concept on concept.concept_id = CONCEPT_ANCESTOR.ancestor_concept_id 
 where
     ancestor_concept_id IN (317009, 4299535, 439727, 201820, 255573, 4063381, 316866, 4028244, 438112)
 group by
