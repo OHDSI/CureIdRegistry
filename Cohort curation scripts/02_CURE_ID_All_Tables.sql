cdbc76b7a342dbb82fbcabdaa5e45bc7aafd2516
smkordik78 <67656275+smkordik78@users.noreply.github.com>
2024-02-16 09:34:52 -0600

    Fixing keyword capitalization, aliasing and parentheses

--- Cohort curation scripts/02_CURE_ID_All_Tables.sql
+++ Cohort curation scripts/02_CURE_ID_All_Tables.sql
@@ -28,16 +28,31 @@ DROP TABLE IF EXISTS [Results].[CURE_ID_Device_Exposure];
 
 --Load person table
 SELECT
-    pe.person_id, pe.gender_concept_id, pe.year_of_birth, pe.month_of_birth, pe.day_of_birth, pe.birth_datetime, pe.race_concept_id, pe.ethnicity_concept_id, pe.location_id, pe.provider_id, pe.care_site_id, NULL
-        as person_source_value, NULL as gender_source_value, pe.gender_source_concept_id, NULL as
-    race_source_value, pe.race_source_concept_id, NULL as ethnicity_source_value, pe.ethnicity_source_concept_id
+    pe.person_id,
+    pe.gender_concept_id,
+    pe.year_of_birth,
+    pe.month_of_birth,
+    pe.day_of_birth,
+    pe.birth_datetime,
+    pe.race_concept_id,
+    pe.ethnicity_concept_id,
+    pe.location_id,
+    pe.provider_id,
+    pe.care_site_id,
+    NULL AS person_source_value,
+    NULL AS gender_source_value,
+    pe.gender_source_concept_id,
+    NULL AS race_source_value,
+    pe.race_source_concept_id,
+    NULL AS ethnicity_source_value,
+    pe.ethnicity_source_concept_id
 INTO [Results].[CURE_ID_Person]
-FROM person pe
-INNER JOIN [Results].[CURE_ID_Cohort] coh
+FROM person AS pe
+INNER JOIN [Results].[CURE_ID_Cohort] AS coh
     ON pe.person_id = coh.person_id
 
 --Load measurements table
-SELECT distinct
+SELECT DISTINCT
     m.measurement_id,
     m.person_id,
     m.measurement_concept_id,
@@ -54,90 +69,168 @@ SELECT distinct
     m.provider_id,
     m.visit_occurrence_id,
     m.visit_detail_id,
-    NULL as measurement_source_value,
+    NULL AS measurement_source_value,
     m.measurement_source_concept_id,
-    NULL as unit_source_value,
+    NULL AS unit_source_value,
     m.unit_source_concept_id,
-    NULL as value_source_value,
+    NULL AS value_source_value,
     m.measurement_event_id,
     m.meas_event_field_concept_id
 INTO [Results].[CURE_ID_Measurement]
-FROM measurement m
-INNER JOIN [Results].[CURE_ID_Cohort] coh
+FROM measurement AS m
+INNER JOIN [Results].[CURE_ID_Cohort] AS coh
     ON
         m.person_id = coh.person_id
-        AND m.measurement_date BETWEEN DATEADD(day, -1, coh.visit_start_date) AND DATEADD(day, 1, coh.visit_end_date)
+        AND m.measurement_date BETWEEN DATEADD(DAY, -1, coh.visit_start_date) AND DATEADD(DAY, 1, coh.visit_end_date)
 INNER JOIN CONCEPT_ANCESTOR
     ON descendant_concept_id = m.measurement_concept_id
 INNER JOIN [Results].[cure_id_concepts]
-    on ancestor_concept_id = concept_id
-where
+    ON ancestor_concept_id = concept_id
+WHERE
     domain = 'Measurement'
-    and (include_descendants = 'TRUE' or ancestor_concept_id = descendant_concept_id)
+    AND (include_descendants = 'TRUE' OR ancestor_concept_id = descendant_concept_id)
 
 --Load drug_exposure table
-SELECT d.drug_exposure_id, d.person_id, d.drug_concept_id, d.drug_exposure_start_date, d.drug_exposure_start_datetime, d.drug_exposure_end_date, d.drug_exposure_end_datetime, d.verbatim_end_date, d.drug_type_concept_id, d.stop_reason, d.refills, d.quantity, d.days_supply, d.sig, d.route_concept_id, d.lot_number, d.provider_id, d.visit_occurrence_id, d.visit_detail_id, NULL as drug_source_value, d.drug_source_concept_id, NULL as route_source_value, NULL as dose_unit_source_value
+SELECT
+    d.drug_exposure_id,
+    d.person_id,
+    d.drug_concept_id,
+    d.drug_exposure_start_date,
+    d.drug_exposure_start_datetime,
+    d.drug_exposure_end_date,
+    d.drug_exposure_end_datetime,
+    d.verbatim_end_date,
+    d.drug_type_concept_id,
+    d.stop_reason,
+    d.refills,
+    d.quantity,
+    d.days_supply,
+    d.sig,
+    d.route_concept_id,
+    d.lot_number,
+    d.provider_id,
+    d.visit_occurrence_id,
+    d.visit_detail_id,
+    NULL AS drug_source_value,
+    d.drug_source_concept_id,
+    NULL AS route_source_value,
+    NULL AS dose_unit_source_value
 INTO [Results].[CURE_ID_Drug_exposure]
-FROM drug_exposure d
-INNER JOIN [Results].[CURE_ID_Cohort] coh
+FROM drug_exposure AS d
+INNER JOIN [Results].[CURE_ID_Cohort] AS coh
     ON
         d.person_id = coh.person_id
-        AND d.drug_exposure_start_date BETWEEN DATEADD(day, -1, coh.visit_start_date) AND DATEADD(day, 1, coh.visit_end_date)
-INNER JOIN concept con
+        AND d.drug_exposure_start_date BETWEEN DATEADD(DAY, -1, coh.visit_start_date) AND DATEADD(DAY, 1, coh.visit_end_date)
+INNER JOIN concept AS con
     ON con.concept_id = d.drug_concept_id
 ORDER BY
     coh.person_id,
     d.drug_exposure_start_datetime
 
 --Load Death table
-SELECT death.person_id, death.death_date, death.death_datetime, death.death_type_concept_id, death.cause_concept_id, NULL as cause_source_value, death.cause_source_concept_id
+SELECT
+    death.person_id,
+    death.death_date,
+    death.death_datetime,
+    death.death_type_concept_id,
+    death.cause_concept_id,
+    NULL AS cause_source_value,
+    death.cause_source_concept_id
 INTO [Results].[CURE_ID_Death]
 FROM death
-INNER JOIN [Results].[CURE_ID_Cohort] coh
+INNER JOIN [Results].[CURE_ID_Cohort] AS coh
     ON death.person_id = coh.person_id
 
 --Load Observation data
 SELECT DISTINCT
-    o.observation_id, o.person_id, o.observation_concept_id, o.observation_date, o.observation_datetime, o.observation_type_concept_id, o.value_as_number, o.value_as_string, o.value_as_concept_id, o.qualifier_concept_id, o.unit_concept_id, o.provider_id, o.visit_occurrence_id, o.visit_detail_id, NULL as observation_source_value, o.observation_source_concept_id,
-    NULL as unit_source_value, NULL as qualifier_source_value, NULL as value_source_value, o.observation_event_id, o.obs_event_field_concept_id
+    o.observation_id,
+    o.person_id,
+    o.observation_concept_id,
+    o.observation_date,
+    o.observation_datetime,
+    o.observation_type_concept_id,
+    o.value_as_number,
+    o.value_as_string,
+    o.value_as_concept_id,
+    o.qualifier_concept_id,
+    o.unit_concept_id,
+    o.provider_id,
+    o.visit_occurrence_id,
+    o.visit_detail_id,
+    NULL AS observation_source_value,
+    o.observation_source_concept_id,
+    NULL AS unit_source_value,
+    NULL AS qualifier_source_value,
+    NULL AS value_source_value,
+    o.observation_event_id,
+    o.obs_event_field_concept_id
 INTO [Results].[CURE_ID_Observation]
-FROM observation o
-INNER JOIN [Results].[CURE_ID_Cohort] coh
+FROM observation AS o
+INNER JOIN [Results].[CURE_ID_Cohort] AS coh
     ON
         o.person_id = coh.person_id
         AND o.observation_date BETWEEN DATEADD(YEAR, -1, coh.visit_start_date) AND DATEADD(YEAR, 1, coh.visit_end_date)
 INNER JOIN CONCEPT_ANCESTOR
     ON descendant_concept_id = o.observation_concept_id
 INNER JOIN [Results].[cure_id_concepts]
-    on ancestor_concept_id = concept_id
-where
+    ON ancestor_concept_id = concept_id
+WHERE
     domain = 'Observation'
-    and (include_descendants = 'TRUE' or ancestor_concept_id = descendant_concept_id)
-
+    AND (include_descendants = 'TRUE' OR ancestor_concept_id = descendant_concept_id)
 
 --Load Procedure Occurrence Table
-SELECT p.procedure_occurrence_id, p.person_id, p.procedure_concept_id, p.procedure_date, p.procedure_datetime, p.procedure_end_date, p.procedure_end_datetime, p.procedure_type_concept_id, p.modifier_concept_id, p.quantity, p.provider_id, p.visit_occurrence_id, p.visit_detail_id, NULL as procedure_source_value, p.procedure_source_concept_id, NULL as modifier_source_value
+SELECT
+    p.procedure_occurrence_id,
+    p.person_id,
+    p.procedure_concept_id,
+    p.procedure_date,
+    p.procedure_datetime,
+    p.procedure_end_date,
+    p.procedure_end_datetime,
+    p.procedure_type_concept_id,
+    p.modifier_concept_id,
+    p.quantity,
+    p.provider_id,
+    p.visit_occurrence_id,
+    p.visit_detail_id,
+    NULL AS procedure_source_value,
+    p.procedure_source_concept_id,
+    NULL AS modifier_source_value
 INTO [Results].[CURE_ID_Procedure_Occurrence]
-FROM procedure_occurrence p
-INNER JOIN [Results].[CURE_ID_Cohort] coh
+FROM procedure_occurrence AS p
+INNER JOIN [Results].[CURE_ID_Cohort] AS coh
     ON
         p.person_id = coh.person_id
-        AND p.procedure_date BETWEEN DATEADD(day, -1, coh.visit_start_date) AND DATEADD(day, 1, coh.visit_end_date)
+        AND p.procedure_date BETWEEN DATEADD(DAY, -1, coh.visit_start_date) AND DATEADD(DAY, 1, coh.visit_end_date)
 INNER JOIN CONCEPT_ANCESTOR
     ON descendant_concept_id = p.procedure_concept_id
 INNER JOIN [Results].[cure_id_concepts]
-    on ancestor_concept_id = concept_id
-where
+    ON ancestor_concept_id = concept_id
+WHERE
     domain = 'Procedure'
-    and (include_descendants = 'TRUE' or ancestor_concept_id = descendant_concept_id)
+    AND (include_descendants = 'TRUE' OR ancestor_concept_id = descendant_concept_id)
 
 --Load Condition Occurrence table
 SELECT
-    c.condition_occurrence_id, c.person_id, c.condition_concept_id, c.condition_start_date, c.condition_start_datetime, c.condition_end_date, c.condition_end_datetime, c.condition_type_concept_id, c.condition_status_concept_id, c.stop_reason, c.provider_id, c.visit_occurrence_id, c.visit_detail_id, NULL as condition_source_value, c.condition_source_concept_id,
-    NULL as condition_status_source_value
+    c.condition_occurrence_id,
+    c.person_id,
+    c.condition_concept_id,
+    c.condition_start_date,
+    c.condition_start_datetime,
+    c.condition_end_date,
+    c.condition_end_datetime,
+    c.condition_type_concept_id,
+    c.condition_status_concept_id,
+    c.stop_reason,
+    c.provider_id,
+    c.visit_occurrence_id,
+    c.visit_detail_id,
+    NULL AS condition_source_value,
+    c.condition_source_concept_id,
+    NULL AS condition_status_source_value
 INTO [Results].[CURE_ID_Condition_Occurrence]
-FROM condition_occurrence c
-INNER JOIN [Results].[CURE_ID_Cohort] coh
+FROM condition_occurrence AS c
+INNER JOIN [Results].[CURE_ID_Cohort] AS coh
     ON
         c.person_id = coh.person_id
         AND (
@@ -148,38 +241,69 @@ INNER JOIN [Results].[CURE_ID_Cohort] coh
 INNER JOIN CONCEPT_ANCESTOR
     ON descendant_concept_id = c.condition_concept_id
 INNER JOIN [Results].[cure_id_concepts]
-    on ancestor_concept_id = concept_id
-where
+    ON ancestor_concept_id = concept_id
+WHERE
     domain = 'Condition'
-    and (include_descendants = 'TRUE' or ancestor_concept_id = descendant_concept_id)
-
+    AND (include_descendants = 'TRUE' OR ancestor_concept_id = descendant_concept_id)
 
 --Load Visit Occurrence table
 SELECT DISTINCT
-    v.visit_occurrence_id, v.person_id, v.visit_concept_id, v.visit_start_date, v.visit_start_datetime, v.visit_end_date, v.visit_end_datetime, v.visit_type_concept_id, v.provider_id, v.care_site_id,
-    NULL as visit_source_value, v.visit_source_concept_id, v.admitted_from_concept_id,
-    NULL as admitted_from_source_value, v.discharged_to_concept_id, NULL as discharged_to_source_value,
+    v.visit_occurrence_id,
+    v.person_id,
+    v.visit_concept_id,
+    v.visit_start_date,
+    v.visit_start_datetime,
+    v.visit_end_date,
+    v.visit_end_datetime,
+    v.visit_type_concept_id,
+    v.provider_id,
+    v.care_site_id,
+    NULL AS visit_source_value,
+    v.visit_source_concept_id,
+    v.admitted_from_concept_id,
+    NULL AS admitted_from_source_value,
+    v.discharged_to_concept_id,
+    NULL AS discharged_to_source_value,
     v.preceding_visit_occurrence_id
 INTO [Results].[CURE_ID_Visit_Occurrence]
-FROM visit_occurrence v
-INNER JOIN [Results].[CURE_ID_Cohort] coh
+FROM visit_occurrence AS v
+INNER JOIN [Results].[CURE_ID_Cohort] AS coh
     ON
         v.person_id = coh.person_id
         AND v.visit_occurrence_id = coh.visit_occurrence_id
 WHERE v.visit_start_date >= '2020-03-01'
 
 --Load Device Exposure table
-SELECT dev.device_exposure_id, dev.person_id, dev.device_concept_id, dev.device_exposure_start_date, dev.device_exposure_start_datetime, dev.device_exposure_end_date, dev.device_exposure_end_datetime, dev.device_type_concept_id, dev.unique_device_id, dev.production_id, dev.quantity, dev.provider_id, dev.visit_occurrence_id, dev.visit_detail_id, NULL as device_source_value, dev.device_source_concept_id, dev.unit_concept_id, NULL as unit_source_value, dev.unit_source_concept_id
+SELECT
+    dev.device_exposure_id,
+    dev.person_id,
+    dev.device_concept_id,
+    dev.device_exposure_start_date,
+    dev.device_exposure_start_datetime,
+    dev.device_exposure_end_date,
+    dev.device_exposure_end_datetime,
+    dev.device_type_concept_id,
+    dev.unique_device_id,
+    dev.production_id,
+    dev.quantity,
+    dev.provider_id,
+    dev.visit_occurrence_id,
+    dev.visit_detail_id,
+    NULL AS device_source_value,
+    dev.device_source_concept_id,
+    dev.unit_concept_id,
+    NULL AS unit_source_value,
+    dev.unit_source_concept_id
 INTO [Results].[CURE_ID_Device_Exposure]
-FROM device_exposure dev
-INNER JOIN [Results].[CURE_ID_Cohort] coh
+FROM device_exposure AS dev
+INNER JOIN [Results].[CURE_ID_Cohort] AS coh
     ON
         dev.person_id = coh.person_id
-        AND dev.device_exposure_start_date BETWEEN DATEADD(day, -1, coh.visit_start_date) AND DATEADD(day, 1, coh.visit_end_date)
+        AND dev.device_exposure_start_date BETWEEN DATEADD(DAY, -1, coh.visit_start_date) AND DATEADD(DAY, 1, coh.visit_end_date)
 INNER JOIN CONCEPT_ANCESTOR
     ON descendant_concept_id = dev.device_concept_id
 INNER JOIN [Results].[cure_id_concepts]
-    on ancestor_concept_id = concept_id
-where
+    ON ancestor_concept_id = concept_id
+WHERE
     domain = 'Device'
-    and (include_descendants = 'TRUE' or ancestor_concept_id = descendant_concept_id)
+    AND (include_descendants = 'TRUE' OR ancestor_concept_id = descendant_concept_id)
