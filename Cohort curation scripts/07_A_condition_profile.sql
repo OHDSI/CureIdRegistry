8c311086c8658331c1681904d5a1419362fcbe4e
smkordik78 <67656275+smkordik78@users.noreply.github.com>
2024-02-16 09:39:37 -0600

    Fixing keyword capitalization, aliasing and parentheses

--- Cohort curation scripts/07_A_condition_profile.sql
+++ Cohort curation scripts/07_A_condition_profile.sql
@@ -15,48 +15,46 @@ Dependencies:
 
 USE YOUR_DATABASE;
 
----Profile condition prevalence in cohort by individual condition concept
----Condition counts include descendants 
-select
-    concept_name as condition_name,
+--Profile condition prevalence in cohort by individual condition concept
+--Condition counts include descendants 
+SELECT
+    concept_name AS condition_name,
     ancestor_concept_id,
-    count(distinct c.person_id) as persons_with_condition_or_descendant,
-    100 * count(distinct c.person_id) / (select count(distinct person_id) from [Results].[deident_CURE_ID_Person]) as percent_persons_with_condition
-from
-    [Results].[deident_CURE_ID_Condition_Occurrence] c
-inner join CONCEPT_ANCESTOR
-    on
+    COUNT(DISTINCT c.person_id) AS persons_with_condition_or_descendant,
+    100 * COUNT(DISTINCT c.person_id) / (SELECT COUNT(DISTINCT person_id) FROM [Results].[deident_CURE_ID_Person]) AS percent_persons_with_condition
+FROM
+    [Results].[deident_CURE_ID_Condition_Occurrence] AS c
+INNER JOIN CONCEPT_ANCESTOR
+    ON
         descendant_concept_id = c.condition_concept_id
-        and ancestor_concept_id in
-        (select condition_concept_id from [Results].[deident_CURE_ID_Condition_Occurrence])
-left join CONCEPT
-    on concept.concept_id = CONCEPT_ANCESTOR.ancestor_concept_id
-group by
+        AND ancestor_concept_id IN
+        (SELECT condition_concept_id FROM [Results].[deident_CURE_ID_Condition_Occurrence])
+LEFT JOIN CONCEPT
+    ON concept.concept_id = CONCEPT_ANCESTOR.ancestor_concept_id
+GROUP BY
     ancestor_concept_id,
     concept_name
-order by persons_with_condition_or_descendant desc
+ORDER BY persons_with_condition_or_descendant DESC
 
-
-
----Profile condition prevalence in cohort by parent condition concepts
-select
-    concept_name as condition_name,
+--Profile condition prevalence in cohort by parent condition concepts
+SELECT
+    concept_name AS condition_name,
     ancestor_concept_id,
-    count(distinct c.person_id) as persons_with_condition,
-    100 * count(distinct c.person_id) / (select count(distinct person_id) from [Results].[deident_CURE_ID_Person]) as percent_persons_with_condition
-from
-    [Results].[deident_CURE_ID_Condition_Occurrence] c
-left join
+    COUNT(DISTINCT c.person_id) AS persons_with_condition,
+    100 * COUNT(DISTINCT c.person_id) / (SELECT COUNT(DISTINCT person_id) FROM [Results].[deident_CURE_ID_Person]) AS percent_persons_with_condition
+FROM
+    [Results].[deident_CURE_ID_Condition_Occurrence] AS c
+LEFT JOIN
     CONCEPT_ANCESTOR
-    on
+    ON
         descendant_concept_id = c.condition_concept_id
 
 INNER JOIN [Results].[cure_id_concepts]
-    on ancestor_concept_id = concept_id
-where
+    ON ancestor_concept_id = concept_id
+WHERE
     domain = 'Condition'
-    and (include_descendants = 'TRUE' or ancestor_concept_id = descendant_concept_id)
-group by
+    AND (include_descendants = 'TRUE' OR ancestor_concept_id = descendant_concept_id)
+GROUP BY
     ancestor_concept_id,
     concept_name
-order by persons_with_condition desc
+ORDER BY persons_with_condition DESC
