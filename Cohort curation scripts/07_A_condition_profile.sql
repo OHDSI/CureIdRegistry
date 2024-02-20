14d7d5dd6795ed749c86dd24be906d883a9f5e5a
smkordik78 <67656275+smkordik78@users.noreply.github.com>
2024-02-20 11:54:01 -0600

    Added table references to avoid ambiguity

--- Cohort curation scripts/07_A_condition_profile.sql
+++ Cohort curation scripts/07_A_condition_profile.sql
@@ -18,43 +18,40 @@ USE YOUR_DATABASE;
 --Profile condition prevalence in cohort by individual condition concept
 --Condition counts include descendants 
 SELECT
-    concept_name AS condition_name,
-    ancestor_concept_id,
+    CONCEPT.concept_name AS condition_name,
+    CONCEPT_ANCESTOR.ancestor_concept_id,
     COUNT(DISTINCT c.person_id) AS persons_with_condition_or_descendant,
-    100 * COUNT(DISTINCT c.person_id) / (SELECT COUNT(DISTINCT person_id) FROM [Results].[deident_CURE_ID_Person]) AS percent_persons_with_condition
+    100 * COUNT(DISTINCT c.person_id) / (SELECT COUNT(DISTINCT c.person_id) FROM [Results].[deident_CURE_ID_Person]) AS percent_persons_with_condition
 FROM
     [Results].[deident_CURE_ID_Condition_Occurrence] AS c
 INNER JOIN CONCEPT_ANCESTOR
     ON
-        descendant_concept_id = c.condition_concept_id
-        AND ancestor_concept_id IN
+        CONCEPT_ANCESTOR.descendant_concept_id = c.condition_concept_id
+        AND CONCEPT_ANCESTOR.ancestor_concept_id IN
         (SELECT condition_concept_id FROM [Results].[deident_CURE_ID_Condition_Occurrence])
 LEFT JOIN CONCEPT
-    ON concept.concept_id = CONCEPT_ANCESTOR.ancestor_concept_id
-GROUP BY
-    ancestor_concept_id,
-    concept_name
+    ON CONCEPT.concept_id = CONCEPT_ANCESTOR.ancestor_concept_id
+GROUP BY CONCEPT_ANCESTOR.ancestor_concept_id, CONCEPT.concept_name
 ORDER BY persons_with_condition_or_descendant DESC
 
 --Profile condition prevalence in cohort by parent condition concepts
 SELECT
-    concept_name AS condition_name,
-    ancestor_concept_id,
+    [Results].[cure_id_concepts].concept_name AS condition_name,
+    CONCEPT_ANCESTOR.ancestor_concept_id,
     COUNT(DISTINCT c.person_id) AS persons_with_condition,
-    100 * COUNT(DISTINCT c.person_id) / (SELECT COUNT(DISTINCT person_id) FROM [Results].[deident_CURE_ID_Person]) AS percent_persons_with_condition
+    100 * COUNT(DISTINCT c.person_id) / (SELECT COUNT(DISTINCT c.person_id) FROM [Results].[deident_CURE_ID_Person]) AS percent_persons_with_condition
 FROM
     [Results].[deident_CURE_ID_Condition_Occurrence] AS c
-LEFT JOIN
-    CONCEPT_ANCESTOR
-    ON
-        descendant_concept_id = c.condition_concept_id
-
+LEFT JOIN CONCEPT_ANCESTOR
+    ON CONCEPT_ANCESTOR.descendant_concept_id = c.condition_concept_id
 INNER JOIN [Results].[cure_id_concepts]
-    ON ancestor_concept_id = concept_id
+    ON CONCEPT_ANCESTOR.ancestor_concept_id = [Results].[cure_id_concepts].concept_id
 WHERE
-    domain = 'Condition'
-    AND (include_descendants = 'TRUE' OR ancestor_concept_id = descendant_concept_id)
+    [Results].[cure_id_concepts].domain = 'Condition'
+    AND (
+        [Results].[cure_id_concepts].include_descendants = 'TRUE'
+        OR CONCEPT_ANCESTOR.ancestor_concept_id = CONCEPT_ANCESTOR.descendant_concept_id
+    )
 GROUP BY
-    ancestor_concept_id,
-    concept_name
+    CONCEPT_ANCESTOR.ancestor_concept_id, [Results].[cure_id_concepts].concept_name
 ORDER BY persons_with_condition DESC
